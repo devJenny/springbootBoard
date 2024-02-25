74423589f24eedf40fbc02b1289493dca9f703f9
devjenny <devje@naver.com>
2024-02-25 23:37:37 +0900

    Create: Entity directory & `Article.java`

---
+++ src/main/java/com/example/springbootBoard/entity/Article.java
@@ -0,0 +1,32 @@
+package com.example.springbootBoard.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity // 엔티티 지정
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Article {
+
+    @Id // id 필드 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name="title", nullable = false) // 'title'이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @Builder // 빌더 패턴 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
