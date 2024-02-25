3cc32cf74ab074b30583de8d75b78c009b3478a6
devjenny <devje@naver.com>
2024-02-25 22:53:28 +0900

    Create gradle project

---
+++ src/test/java/com/example/springbootBoard/SpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
