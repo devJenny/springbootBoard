483075878fb893d424c9617b4fef164eaac602af
devjenny <devje@naver.com>
2024-03-02 01:06:25 +0900

    test: git dev test 1

--- src/main/java/com/example/springbootBoard/TestPage.java
+++ src/main/java/com/example/springbootBoard/TestPage.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestPage {
     // git dev 생성 테스트
     // git dev 생성 테스트
-    // git dev 생성 테스트
+
 
     @GetMapping("/")
     public String test() {
