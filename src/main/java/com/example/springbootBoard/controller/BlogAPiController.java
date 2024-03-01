df8631b1c3384ecad686578ccd6d2d0ffc030724
devjenny <devje@naver.com>
2024-03-01 23:02:50 +0900

    Chore: test

--- src/main/java/com/example/springbootBoard/controller/BlogAPiController.java
+++ src/main/java/com/example/springbootBoard/controller/BlogAPiController.java
@@ -29,6 +29,7 @@ public class BlogAPiController {
                 .body(savedArticle);
     }
 
+    // test
     @GetMapping
     @Comment("전체 조회")
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
