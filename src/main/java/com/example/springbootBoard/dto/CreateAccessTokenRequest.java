84b210bf099a7ae0a347854f2957eb5ee0571b72
devjenny <devje@naver.com>
2024-03-01 23:01:15 +0900

    Chore: JWT refresh token

--- src/main/java/com/example/springbootBoard/dto/CreateAccessTokenRequest.java
+++ src/main/java/com/example/springbootBoard/dto/CreateAccessTokenRequest.java
@@ -1,12 +1,10 @@
 package com.example.springbootBoard.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class AddUserRequest {
-    private String email;
-    private String password;
+public class CreateAccessTokenRequest {
+    private String refreshToken;
 }
---
+++ src/main/java/com/example/springbootBoard/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.example.springbootBoard.service;
+
+import com.example.springbootBoard.entity.RefreshToken;
+import com.example.springbootBoard.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+}
