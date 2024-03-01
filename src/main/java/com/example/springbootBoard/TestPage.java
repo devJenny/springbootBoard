27e23c9b402c75dbd2eb4a5a01bc4a248cedde83
devjenny <devje@naver.com>
2024-03-02 00:10:44 +0900

    test: git dev re test

--- src/main/java/com/example/springbootBoard/TestPage.java
+++ src/main/java/com/example/springbootBoard/TestPage.java
@@ -1,6 +1,14 @@
 package com.example.springbootBoard;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestPage {
     // git dev 생성 테스트
     // git dev 생성 테스트
+    @GetMapping("/")
+    public String test() {
+        return "test";
+    }
 }
